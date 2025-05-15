fc3c4a7ec8a3c4236eb242de190655abb94f9cf1
oussamachakeur <157352982+oussamachakeur@users.noreply.github.com>
2025-05-15 20:19:26 +0800

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -14,16 +14,22 @@ ORDER BY
 
 
 -- Query 2: Rank properties by total bookings
+-- Query: Rank properties by total bookings using ROW_NUMBER()
 SELECT 
-    p.id AS property_id,
-    p.title,
-    COUNT(b.id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
-FROM 
-    properties p
-LEFT JOIN 
-    bookings b ON p.id = b.property_id
-GROUP BY 
-    p.id, p.title
-ORDER BY 
-    booking_rank;
+    property_id,
+    title,
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank
+FROM (
+    SELECT 
+        p.id AS property_id,
+        p.title,
+        COUNT(b.id) AS total_bookings
+    FROM 
+        properties p
+    LEFT JOIN 
+        bookings b ON p.id = b.property_id
+    GROUP BY 
+        p.id, p.title
+) AS property_booking_counts
+ORDER BY booking_rank;
