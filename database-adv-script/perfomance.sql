e6116dd6b84582336257c30e606c5802f091a33a
oussamachakeur <157352982+oussamachakeur@users.noreply.github.com>
2025-05-15 20:28:51 +0800

    Create perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,27 @@
+-- Analyze the original query
+EXPLAIN ANALYZE
+SELECT 
+    b.id AS booking_id,
+    u.id AS user_id,
+    u.name AS user_name,
+    p.id AS property_id,
+    p.name AS property_name,
+    pay.id AS payment_id,
+    pay.amount
+FROM bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON b.payment_id = pay.id;
+
+
+-- Analyze optimized query
+EXPLAIN ANALYZE
+SELECT 
+    b.id AS booking_id,
+    u.name AS user_name,
+    p.name AS property_name,
+    pay.amount
+FROM bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+LEFT JOIN payments pay ON b.payment_id = pay.id;
